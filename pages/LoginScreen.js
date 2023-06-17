2d0fc5ec61e61f163b954f8f5513a9bbedd2ef40
Joata Oliveira <joatakesley@hotmail.com>
2023-06-17 13:27:37 -0300

    integracao tela de login para captacao de token

--- pages/LoginScreen.js
+++ pages/LoginScreen.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   View,
   Image,
@@ -8,9 +8,47 @@ import {
   StyleSheet
 } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
+import { useContext } from 'react'
+import AppContext from '../context/AppContext'
+import PegarToken from '../api/pegar_token'
 
 const LoginScreen = () => {
   const navigation = useNavigation()
+  const { state, dispatch } = useContext(AppContext)
+  const [isLoading, setLoading] = useState(false)
+  const controller = new PegarToken()
+  const [usuario, setUsuario] = useState('')
+  const [senha, setSenha] = useState('')
+
+  const getUsersNaAPI = async () => {
+    try {
+      setLoading(true)
+      token = await controller.authUser(usuario, senha)
+
+      if (token != null) {
+        console.log('entrei')
+
+        var novaCredencial = {
+          login: usuario,
+          token: token.access_token
+        }
+
+        console.log(novaCredencial)
+
+        /*dispatch({
+          type: 'authUser', //especifica a ação
+          payload: novaCredencial //dado necessário para ação
+        })*/
+        navigation.navigate('Tabs')
+      }
+    } catch (error) {
+      console.log('deu ruim')
+      console.log(error)
+      alert('Falha ao acessar servidor. Tente novamente mais tarde!')
+    } finally {
+      setLoading(false)
+    }
+  }
 
   return (
     <View style={styles.container}>
@@ -18,13 +56,26 @@ const LoginScreen = () => {
       <Text style={styles.title}>Processos OPME</Text>
       <TextInput
         placeholder="Usuário"
+        value={usuario}
         style={styles.input}
         textContentType="username"
+        autoCapitalize="none"
+        onChangeText={value => {
+          setUsuario(value)
+        }}
       />
-      <TextInput placeholder="Senha" secureTextEntry style={styles.input} />
+      <TextInput
+        placeholder="Senha"
+        secureTextEntry
+        value={senha}
+        style={styles.input}
+        onChangeText={value => {
+          setSenha(value)
+        }}
+      />
       <TouchableOpacity
         style={styles.loginButton}
-        onPress={() => navigation.navigate('Tabs')}
+        onPress={() => getUsersNaAPI()}
       >
         <Text style={styles.loginText}>Entrar</Text>
       </TouchableOpacity>
