b2b832e970e5b92fc2d5c09e8872b26185efd1c2
Daniel Guerra <daniel.engcomp@gmail.com>
2023-06-19 15:34:23 -0300

    implementação delete e patch

--- api/seguradoController.js
+++ api/seguradoController.js
@@ -6,8 +6,8 @@ class SeguradoControler {
 
         try {
 
-            //const uri = 'http://192.168.1.34:3000/segurados'
-            const uri = 'http://172.20.3.232:3000/segurados'
+            const uri = 'http://192.168.1.34:3000/segurados'
+            //const uri = 'http://172.20.3.232:3000/segurados'
             const response = await fetch(uri, {
                 method: 'GET',
                 headers: {
@@ -32,38 +32,83 @@ class SeguradoControler {
 
     }
 
-    saveSegurado = async (token,user) => {
+    saveSegurado = async (token, user) => {
         try {
             console.log("user do salvar")
             //console.log(token)
             //console.log(user)
-            const uri = 'http://172.20.3.232:3000/segurados'
+            const uri = 'http://192.168.1.34:3000/segurados'
             const response = await fetch(uri, {
                 method: 'POST',
                 headers: {
                     Accept: 'application/json',
                     'Content-Type': 'application/json',
                     'Authorization': 'Bearer ' + token['access_token'],
-    
+
                 },
                 body: JSON.stringify({
                     nomeSegurado: user.nomeSegurado,
                     dataNascimento: user.dataNascimento,
                     carteira: {
                         carteira: user.carteira,
-                        //carteira: '00629999989999995',
                         viaCarteira: user.viaCarteira
-                        //viaCarteira: 1
                     }
                 })
             });
             console.log(response.status);
             const newClient = await response.json();
-            console.log(newClient);
+            //console.log(newClient);
         } catch (error) {
             console.error(error);
         }
     }
+
+    removeSegurado = async (token, user) => {
+        try {
+            console.log("user do renove")
+            const uri = 'http://192.168.1.34:3000/segurados/' + user.id
+            const response = await fetch(uri, {
+                method: 'DELETE',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + token['access_token'],
+                }
+            });
+            console.log(response.status);
+            this.pegarSegurados(token);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    updateSegurado = async (token, user) => {
+        try {
+            console.log("update do salvar")
+            //console.log(token)
+            //console.log(user)
+            const uri = 'http://192.168.1.34:3000/segurados/' + user.id
+            const response = await fetch(uri, {
+                method: 'PATCH',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + token['access_token'],
+                },
+                body: JSON.stringify({
+                    nomeSegurado: user.nomeSegurado,
+                    dataNascimento: user.dataNascimento,
+                })
+            });
+            console.log(response.status);
+            const newClient = await response.json();
+            //console.log(newClient);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+
 }
 
 
