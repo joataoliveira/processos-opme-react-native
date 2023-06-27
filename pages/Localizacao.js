c162a18626349ea940f722cb7b6c02e48e5d00fb
Joata Oliveira <joatakesley@hotmail.com>
2023-06-26 22:01:24 -0300

    ajustes no layout da tela de localização

--- pages/Localizacao.js
+++ pages/Localizacao.js
@@ -1,40 +1,72 @@
-import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, View, PermissionsAndroid, Button, Platform } from "react-native";
-import * as Location from 'expo-location';
-import {UrlTile} from 'react-native-maps';
-
-import MapView from "react-native-maps";
+import React, { useEffect, useState, useContext } from 'react'
+import {
+  StyleSheet,
+  Text,
+  View,
+  PermissionsAndroid,
+  Button,
+  Platform
+} from 'react-native'
+import * as Location from 'expo-location'
+import { UrlTile } from 'react-native-maps'
+import MapView from 'react-native-maps'
+import AppContext from '../context/AppContext'
 
 const Localizacao = ({ navigation, route }) => {
-    const [location, setLocation] = useState();
-    const [address, setAddress] = useState();
+  const [location, setLocation] = useState()
+  const [address, setAddress] = useState()
+  const { state, dispatch } = useContext(AppContext)
 
-    useEffect(() => {
-        const getPermissions = async () => {
-          let { status } = await Location.requestForegroundPermissionsAsync();
-          if (status !== 'granted') {
-            console.log("Please grant location permissions");
-            return;
-          }
-    
-          let currentLocation = await Location.getCurrentPositionAsync({});
-          setLocation(currentLocation);
-          console.log("Location:");
-          console.log(currentLocation);
-        };
-        getPermissions();
-      }, []); 
+  useEffect(() => {
+    const getPermissions = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        console.log('Please grant location permissions')
+        return
+      }
 
-        
-    return (
-        <View >
-            <MapView style={{with:'100%',height:'100%'}} initialRegion={location} showsUserLocation>
-            <UrlTile maximumZ={19} ></UrlTile>
-            </MapView>
-        </View>
-    )
+      let currentLocation = await Location.getCurrentPositionAsync({})
+      setLocation(currentLocation)
+      //console.log('Location:')
+      //console.log(currentLocation)
+    }
+    getPermissions()
+  }, [])
 
+  return (
+    <View style={styles.container}>
+      <Text style={styles.greeting}>
+        Olá, {state.credentials.login}! você está aqui:
+      </Text>
+      <View style={styles.mapContainer}>
+        <MapView style={styles.map} initialRegion={location} showsUserLocation>
+          <UrlTile maximumZ={19}></UrlTile>
+        </MapView>
+      </View>
+    </View>
+  )
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  greeting: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10
+  },
+  mapContainer: {
+    width: '90%',
+    height: '70%',
+    borderRadius: 10,
+    overflow: 'hidden'
+  },
+  map: {
+    flex: 1
+  }
+})
 
 export { Localizacao }
